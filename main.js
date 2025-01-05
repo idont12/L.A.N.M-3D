93dbad9cdc2647fdc1daf2fc674a5636961029d5
Yarden Zilberman <hrsi9080@gmail.com>
2025-01-05 22:21:16 +0200

    Fixed three.js module resolution by using CDN

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Import Three.js
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.171.0/build/three.module.js';
 import { InteractionManager } from 'three.interactive';
 
 
